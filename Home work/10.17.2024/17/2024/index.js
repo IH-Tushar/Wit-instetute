fb0fdfac652af5d0c888777fea539fcfdb376349
IH-Tushar <mohammodtushar.cb@gmail.com>
2024-10-18 19:15:06 +0600

    solve some mistake

--- Home work/10.17.2024/17/2024/index.js
+++ Home work/10.17.2024/17/2024/index.js
@@ -6,7 +6,7 @@
 
 // Write a function called sumUpToN that takes a number n and returns the sum of all numbers from 1 to n. Use a loop.
 
-    sum = 0;
+    let sum = 0;
     function sumUpTon (n){
         for(let i=1; i<=n; i++){
             sum = sum+=i;
@@ -16,10 +16,10 @@
     console.log(sumUpTon(5));
 // Write a function called sumArray that takes an array of numbers as a parameter and returns the sum of all the numbers in the array.
     let newsum = 0;
-    arr = [2,3,4]
-    function sumArray(){
-        for(let i=0; i<arr.length; i++){
-            newsum = newsum += arr[i];
+    let arr = [2,3,4]
+    function sumArray(newarr){
+        for(let i=0; i<newarr.length; i++){
+            newsum += newarr[i];
         }
         return newsum;
     }
@@ -28,8 +28,8 @@
 // Write a function called countEvenNumbers that takes an array of numbers as a parameter and returns the count of even numbers in the array.
 
 let anotherarr = [10,15,26,2,4,6,8,10,12]
-even = 0;
-    function countEvenNumbers(){
+let even = 0;
+    function countEvenNumbers(anotherarr){
         for(i=0; i<anotherarr.length; i++){
             if(anotherarr[i] % 2 == 0){
                 even++;
@@ -37,15 +37,15 @@ even = 0;
         }
         return even;
     }
-    countEvenNumbers()
+    countEvenNumbers(anotherarr);
     console.log(even)
 
 // Write a function called stringLength that takes a string as a parameter and returns the length of the string.
     let str = "hello world";
     let lengthofstring;
-function stringLength(){
+function stringLength(str){
     lengthofstring = str.length;
     return lengthofstring;
 }
-stringLength();
+stringLength(str);
 console.log(lengthofstring);
